fcc54c5fd8c2bf98e299dce6bc796fedbc33a2b4
Andrea Borruso <aborruso@gmail.com>
2015-10-03 10:06:50 +0200

    update

---
+++ data/dataset.js
@@ -0,0 +1,36 @@
+{
+    "types": {
+        "dataset" : {
+            "pluralLabel": "dataset"
+        }
+    },
+    "items" : [
+        {   "tema_dati_gov":                  "Government e settore pubblico",
+            "label":                 "Numeri telefonici",
+            "formato":            "pdf",
+            "keywords":                "bene;male",
+            "descrizione":                "lorem ipsum sit doler amet, ut secundur",
+            "ufficio":                "Tutti i Dipartimenti e gli Uffici Speciali",
+            "URL_pubblicazione":                "http://pti.regione.sicilia.it/portal/page/portal/PIR_PORTALE/PIR_Elenchitelefonici",
+            "pagina_web":                "http://pti.regione.sicilia.it/portal/page/portal/PIR_PORTALE/PIR_Elenchitelefonici"
+        },
+        {   "tema_dati_gov":                  "Cultura",
+            "label":                 "Schede informative parchi, musei, gallerie e aree archeologiche",
+            "formato":            "html",
+            "keywords":                "cibo;turismo",
+            "descrizione":                "lorem ipsum sit doler amet, ut secundur lorem ipsum sit doler amet, ut secundur",
+            "ufficio":                "Assessorato dei Beni Culturali e dell'Identità Siciliana Dipartimento dei Beni culturali e dell'Identità siciliana",
+            "URL_pubblicazione":                "http://www.regione.sicilia.it/beniculturali/dirbenicult/database/page_musei/pagina_musei.asp?ID=60",
+            "pagina_web":                "http://www.regione.sicilia.it/beniculturali/dirbenicult/database/page_musei/pagina_musei.asp?ID=60"
+        },
+        {   "tema_dati_gov":                  "Ambiente e meteo",
+            "label":                 "Piano di Gestione UNESCO Isole Eolie",
+            "formato":            "html",
+            "keywords":                "cibo;turismo",
+            "descrizione":                "lorem ipsum sit doler amet, ut secundur lorem ipsum sit doler amet, ut secundur",
+            "ufficio":                "Assessorato dei Beni Culturali e dell'Identità Siciliana Dipartimento dei Beni culturali e dell'Identità siciliana",
+            "URL_pubblicazione":                "http://www.regione.sicilia.it/beniculturali/dirbenicult/pianogestioneeolie.html",
+            "pagina_web":                "http://www.regione.sicilia.it/beniculturali/dirbenicult/pianogestioneeolie.html"
+        }
+    ]
+}
