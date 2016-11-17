c72107217b0b7f9d03baebe3bdac962c61b1dc1b
cristianolongo <cristianolongo@gmail.com>
2016-11-17 21:42:48 +0100

    #69 pubblicazione endpoint

--- csv2dcat/generate.sh
+++ csv2dcat/generate.sh
@@ -2,3 +2,5 @@
 
 curl -vs "https://docs.google.com/spreadsheets/d/15cFU9v2x3ACb1IPcVE9Xg3wc5fuay8NAYDXQnoMcOFw/export?format=tsv&exportFormat=tsv&ndplr=1" | \
 php csv2dcat.php >nonportale.owl
+curl -X PUT -H "Content-Type: application/rdf+xml" -u 'cristianolongo'  --data-binary @nonportale.owl http://dydra.com/cristianolongo/dataset-regione-sicilia-ods15/service
+
